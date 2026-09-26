f0cbc2ba37d7c9e213d1e58eabce358f4549fa79
AO5002 <154623452+AO-5002@users.noreply.github.com>
2026-09-25 21:14:16 -0500

    Add fork/exec loop and command-parsing to my shell project. -  Created COMMAND-ENUM with an encode function that encodes user's input to the corresponding ENUM value. - Added forking to the shell so child processes execute the user's command, without overwriting the parent. - Refactored while-loop to read the EXIT command instead of the child-process. - Accidentally forked-bombed my computer a couple of times (thank you macOS for protecting me from myself lol).

--- myshell.c
+++ myshell.c
@@ -4,18 +4,39 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-static const char *commands[] = {"ps", "ls", "test", "exit" };
 
-int main(int argc, char *argv[]) {
-    // if (argc != 2) { fprintf(stderr, "Error: Invalid input!"); return -1; }
-    const int SIZE = 256;
-    char input [SIZE] = "";
+enum CMD { TEST, PD, LS, EXIT, OTHER };
+enum CMD encode(const char *s);
 
-    while (strcmp(input, "exit\n") != 0) {
+int main() {
+    char input [256] = "";
+    while (1) {
         printf("%% ");
-        fgets(input, SIZE, stdin);
+        fgets(input, 256, stdin);
+        if (encode(input) == EXIT) break;
+
+        const int pid = fork();
+        if (pid == 0) {
+            switch (encode(input)) {
+                case TEST: { execv("./cmake-build-debug/test", NULL); _exit(0); }
+                case PD: { execv("./cmake-build-debug/pd", NULL); _exit(0); }
+                case LS: { execv("./cmake-build-debug/ls", NULL); _exit(0); }
+                default: { _exit(0); }
+            }
+        }
+
+        waitpid(pid, NULL, 0);
+        printf("\n");
     }
 
     return 0;
+}
+
+enum CMD encode(const char *s) {
+    if (strcmp(s, "exit\n") == 0) return EXIT;
+    if (strcmp(s, "test\n") == 0) return TEST;
+    return OTHER;
 }
---
+++ pd.c
@@ -0,0 +1,7 @@
+//
+// Created by Andres Ortiz Osorio on 9/25/26.
+//
+
+int main() {
+
+}
